b777f2e23c61bcc08dbdfa82d3ece2a4b4957dbb
Diego Alfonso <dlalfonso57@gmail.com>
2024-01-19 10:35:44 -0500

    fix NavBar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,7 +3,22 @@ import "./navbar.css";
 const Nav = () => {
   return (
     <nav className="wrapperNav">
-      <div className="containerNav"></div>
+      <div className="containerNav">
+        <div className="logoNav">
+            <p>Diego</p>
+            <p>Dev</p>
+        </div>
+        <div className="menuNav">
+            <ul className="itemsNav">
+                <li>About</li>
+                <li>Projects</li>
+                <li>Skills</li>
+            </ul>
+            <button className="buttonNav">
+                Contact me
+            </button>
+        </div>
+      </div>
     </nav>
   );
 };
