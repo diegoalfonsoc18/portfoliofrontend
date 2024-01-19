957aa163a23e3ea1bc02509a7d09715f12f38736
Diego Alfonso <dlalfonso57@gmail.com>
2024-01-19 09:33:16 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import Nav from './components/navbar/index'
+
+function App() {
+  
+
+  return (
+    <Nav/>
+  )
+}
+
+export default App
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,11 @@
+import "./navbar.css";
+
+const Nav = () => {
+  return (
+    <nav className="wrapperNav">
+      <div className="containerNav"></div>
+    </nav>
+  );
+};
+
+export default Nav;
