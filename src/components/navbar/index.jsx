6e75fc9ae8e8d666a4acca4ec6fd56f654161ef8
Diego Alfonso <dlalfonso57@gmail.com>
2024-01-19 20:51:13 -0500

    Add menu responsive

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 import "./navbar.css";
+import iconMobile from "../../icon/menuMobile.png"
+
+
+
 
 const Nav = () => {
+
+    const [menuVisible, setmenuvisible] = useState(false);
+
+  
   return (
     <nav className="wrapperNav">
       <div className="containerNav">
@@ -18,7 +27,22 @@ const Nav = () => {
                 Contact me
             </button>
         </div>
+        <div className="MenuMobile" onClick={() => {
+            setmenuvisible(!menuVisible)
+        }}>
+            <img className="iconMobile" src={iconMobile}></img>
+        </div>
+        
       </div>
+      {menuVisible && (
+            <div className="containerMenuMobile">
+            <ul className="itemMenuMobile">
+                <li>About</li>
+                <li>Projects</li>
+                <li>Skills</li>
+            </ul>
+        </div>
+        )}
     </nav>
   );
 };
